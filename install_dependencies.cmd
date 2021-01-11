a4d1b2c146288c9bb4d52b1a022eb0ccb231de96
korhun <korhun@gmail.com>
2021-01-11 15:06:00 +0300

    bat

---
+++ install_dependencies.cmd
@@ -0,0 +1,4 @@
+pip install pyqt5==5.10.1 --upgrade
+pip install lxml==4.6.2 --upgrade
+pyrcc5 -o libs/resources.py resources.qrc
+pause
--- labelImg.bat
+++ labelImg.bat
@@ -1,1 +1,1 @@
-.\venv\Scripts\python.exe labelImg.py
+python labelImg.py
